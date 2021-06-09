94cf95918b939c6888f442b4609f0cfbe8a7afc2
Yunseo Hwang <yunseo.dev@gmail.com>
2021-06-09 23:41:44 +0900

    Reinitialize frontend
    Signed-off-by: Yunseo Hwang <yunseo.dev@gmail.com>

---
+++ frontend/config/webpack.config.common.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const { webpack } = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+    alias: {
+      '@': path.resolve(__dirname, '../src/'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|woff|woff2|ttf|svg|ico)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, '../build'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  plugins: [
+    new CleanWebpackPlugin({cleanStaleWebpackAssets: false,}),
+    new HtmlWebpackPlugin({template: './public/index.html', filename: 'index.html'})
+  ]
+}
